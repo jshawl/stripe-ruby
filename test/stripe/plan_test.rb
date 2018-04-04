38a69bc64fdc6b21730f52750bcd1e4b36fe82c3
Alexander Thiemann <alexander@stripe.com>
2018-04-04 11:19:36 -0700

    linter fixes

--- test/stripe/plan_test.rb
+++ test/stripe/plan_test.rb
@@ -29,12 +29,12 @@ module Stripe
 
     should "be creatable with metered configuration" do
       plan = Stripe::Plan.create(
-          amount: 5000,
-          interval: "month",
-          name: "Sapphire elite",
-          currency: "usd",
-          id: "sapphire-elite",
-          usage_type: "metered"
+        amount: 5000,
+        interval: "month",
+        name: "Sapphire elite",
+        currency: "usd",
+        id: "sapphire-elite",
+        usage_type: "metered"
       )
       assert_requested :post, "#{Stripe.api_base}/v1/plans"
       assert plan.is_a?(Stripe::Plan)
@@ -42,13 +42,13 @@ module Stripe
 
     should "be creatable with tiered configuration" do
       plan = Stripe::Plan.create(
-          interval: "month",
-          name: "Sapphire elite",
-          currency: "usd",
-          id: "sapphire-elite",
-          billing_scheme: "tiered",
-          tiers_mode: "volume",
-          tiers: [{up_to: 100, amount: 1000}, {up_to: "inf", amount: 2000}]
+        interval: "month",
+        name: "Sapphire elite",
+        currency: "usd",
+        id: "sapphire-elite",
+        billing_scheme: "tiered",
+        tiers_mode: "volume",
+        tiers: [{ up_to: 100, amount: 1000 }, { up_to: "inf", amount: 2000 }]
       )
       assert_requested :post, "#{Stripe.api_base}/v1/plans"
       assert plan.is_a?(Stripe::Plan)
@@ -56,12 +56,12 @@ module Stripe
 
     should "be creatable with transform_usage" do
       plan = Stripe::Plan.create(
-          interval: "month",
-          name: "Sapphire elite",
-          currency: "usd",
-          id: "sapphire-elite",
-          amount: 5000,
-          transform_usage: {round: "up", divide_by: 50}
+        interval: "month",
+        name: "Sapphire elite",
+        currency: "usd",
+        id: "sapphire-elite",
+        amount: 5000,
+        transform_usage: { round: "up", divide_by: 50 }
       )
       assert_requested :post, "#{Stripe.api_base}/v1/plans"
       assert plan.is_a?(Stripe::Plan)
